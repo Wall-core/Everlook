142c9b1ede2aa4fa3bc43c7ba58d3da818befa5a
FlagFlayer <76889547+FlagFlayer@users.noreply.github.com>
2022-10-26 01:09:21 +0300

    Update and rename 20220925022431_world.sql to 20220925222431_world.sql
    Rename to make sure this PR gets merged AFTER Daribon's. Converted all the code to be compatible with the parameters used by Daribon.

--- sql/migrations/20220925222431_world.sql
+++ sql/migrations/20220925222431_world.sql
@@ -3,9 +3,9 @@ delimiter ??
 CREATE PROCEDURE `add_migration`()
 BEGIN
 DECLARE v INT DEFAULT 1;
-SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20220925022431');
+SET v = (SELECT COUNT(*) FROM `migrations` WHERE `id`='20220925222431');
 IF v=0 THEN
-INSERT INTO `migrations` VALUES ('20220925022431');
+INSERT INTO `migrations` VALUES ('20220925222431');
 -- Add your query below.
 
 -- Set Mulgore, Red Cloud Mesa GY for Brambleblade Ravine, Kodo Rock, Campe Narache
@@ -13,11 +13,15 @@ INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (34, 637, 67, 0, 10);
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (34, 221, 67, 0, 10);
 
--- Set Durotar, Valley of Trials GY for Spirit Rock, The Den, Hidden Path, Burning Blade Coven
-INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 637, 67, 0, 10);
-INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 364, 67, 0, 10);
-INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 638, 67, 0, 10);
-INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 365, 67, 0, 10);
+-- Set Durotar, Valley of Trials for Spirit Rock, The Den, Hidden Path, Burning Blade Coven until patch 1.6
+UPDATE `game_graveyard_zone` SET `patch_max`=3 WHERE `id`=709 AND `ghost_zone`=363
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 637, 67, 0, 3);
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 364, 67, 0, 3);
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 638, 67, 0, 3);
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 365, 67, 0, 3);
+
+-- Set Durotar, Valley of Trials graveyard for Durotar from patch 1.6 onward
+INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (709, 14, 67, 4, 10);
 
 -- Set Tirisfal Glades, Deathknell GY for Shadow Grave, Night Web's Hollow
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (94, 2117, 67, 0, 10);
@@ -34,9 +38,6 @@ INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (105, 24, 469, 0, 10);
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (105, 34, 469, 0, 10);
 INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (105, 59, 469, 0, 10);
-
--- Add Razor Hill as a graveyard for Burning Blade Coven to retain the ability to graveyard hop from Valley of Trials to Razor Hill
-INSERT INTO `game_graveyard_zone` (`id`, `ghost_zone`, `faction`, `patch_min`, `patch_max`) VALUES (32, 365, 0, 0, 10);
 
 -- Remove invalid Camp Taurajo graveyard for Stonetalon Mountains
 DELETE FROM `game_graveyard_zone` WHERE `id`=229 AND `ghost_zone`=406;
