05b49e1b1b1cff934b897c337bdf652a66d2803e
Wall-core <high_player_score4@protonmail.com>
2022-10-30 13:49:32 -0400

    Reimplement negative resistance hit modifier

--- src/game/Objects/SpellCaster.cpp
+++ src/game/Objects/SpellCaster.cpp
@@ -583,10 +583,10 @@ float SpellCaster::GetSpellResistChance(Unit const* victim, uint32 schoolMask, b
 
     float resistModHitChance = baseResistance + selfResistance;
 
-#if SUPPORTED_CLIENT_BUILD > CLIENT_BUILD_1_8_4
+/* #if SUPPORTED_CLIENT_BUILD > CLIENT_BUILD_1_8_4
     if ((resistModHitChance < 0.0f) && (baseResistance >= 0.0f))
         resistModHitChance = 0.0f;
-#endif
+#endif */
 
     // Magic vulnerability calculation
     if (resistModHitChance < 0.0f)
