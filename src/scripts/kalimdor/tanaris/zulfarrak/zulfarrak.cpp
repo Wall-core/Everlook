4b664e77ebde833ed7f8dbd67e71b3e5f6609f49
schell244 <>
2022-11-03 22:06:45 +0100

    remove useless log

--- src/scripts/kalimdor/tanaris/zulfarrak/zulfarrak.cpp
+++ src/scripts/kalimdor/tanaris/zulfarrak/zulfarrak.cpp
@@ -435,7 +435,6 @@ struct npc_weegli_blastfuseAI : public ScriptedAI
 
     void OnScriptEventHappened(uint32 /*uiEvent*/ = 0, uint32 /*uiData*/ = 0, WorldObject* /*pInvoker*/ = 0) override
     {
-        sLog.Out(LOG_BASIC, LOG_LVL_MINIMAL, "OnScriptEventHappened de npc_weegli_blastfuse : Destruction porte");
         DestroyDoor();
     }
 
