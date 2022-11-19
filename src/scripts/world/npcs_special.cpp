abc5199744a3479e46f8079a65c1b0d5fc2de613
Wall-core <high_player_score4@protonmail.com>
2022-11-19 14:46:23 -0500

    Actually fix Triage NPCs

--- src/scripts/world/npcs_special.cpp
+++ src/scripts/world/npcs_special.cpp
@@ -453,7 +453,7 @@ void npc_doctorAI::PatientDied(Location* Point)
         else
         {
             // If no player or player abandon quest in progress
-            pPlayer->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);        
+            pPlayer->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
             Reset();
         }
     }
@@ -566,9 +566,9 @@ bool QuestAccept_npc_doctor(Player* pPlayer, Creature* pCreature, Quest const* p
 bool QuestRewarded_npc_doctor(Player* pPlayer, Creature* pCreature, Quest const* pQuest)
 {
     if ((pQuest->GetQuestId() == QUEST_TRIAGE_A) || (pQuest->GetQuestId() == QUEST_TRIAGE_H))
-        pCreature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IN_COMBAT);
-        pCreature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
-
+        pPlayer->SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_PLAYER_CONTROLLED);
+     if (npc_doctorAI* pDocAI = dynamic_cast<npc_doctorAI*>(pCreature->AI()))
+        pDocAI->Reset();
     return true;
 }
 
